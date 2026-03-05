dbd0d8598410291ee550216880fb3dda869fcd66
Kevin Siddhpura <siddhpurakevin@gmail.com>
2026-03-06 00:27:28 +0530

    Did 4.4

---
+++ OOP/L-4/EmployeeManager.java
@@ -0,0 +1,46 @@
+class Employee {
+    String name;
+    String dept;
+
+    Employee(String name, String dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    void displayDetails() {
+        System.out.println("[]: Employee Details");
+        System.out.println("=: Name: " + name);
+        System.out.println("=: Department: " + dept);   
+    }
+}
+
+class Manager extends Employee {
+    String project;
+    int teamSize;
+
+    Manager(String name, String dept, String project, int teamSize) {
+        super(name, dept);
+        this.project = project;
+        this.teamSize = teamSize;
+    }
+
+    void displayDetails() {
+        System.out.println("[]: Manager Details");
+        System.out.println("=: Name: " + name);
+        System.out.println("=: Department: " + dept);   
+        System.out.println("=: Project: " + project);   
+        System.out.println("=: Team Size: " + teamSize);   
+    }
+}
+
+class EmployeeManager {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Yash", "C.E.");
+        employee.displayDetails();
+
+        System.out.println("\n");
+
+        Manager manager = new Manager("Kevin", "C.E.", "Project X", 5);
+        manager.displayDetails();   
+    }
+}
